4c24b5f3e8a70ac5c472c93fbd5e658c40e14386
mikhail <pakmisha9@gmail.com>
2025-02-13 20:44:54 +0500

    initial commit

---
+++ src/test/java/com/diploma/dlp/DlpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diploma.dlp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
